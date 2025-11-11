94858b542c5568a477237290228bde9ea5becf2d
SUDHANSHU GHOSH <shudhanshukumar9713@gmail.com>
2025-11-11 14:09:06 +0530

    day134 authentication register & login basics done.

--- Day133_134/src/routes/auth.routers.js
+++ Day133_134/src/routes/auth.routers.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const userModel = require('../models/user.models.js')
+const express = require("express");
+const userModel = require("../models/user.models.js");
 const router = express.Router();
 
 /*
@@ -9,30 +9,42 @@ GET /auth/user
 GET /auth/logout
 */
 
-router.post('/register', async (req, res) => {
-    const { username, password } = req.body;
-    const user = await userModel.create({
-        username, password
-    })
+router.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  const user = await userModel.create({
+    username,
+    password,
+  });
 
-    res.status(201).json({
-        message: "user registered!",
-        user
-    })
-})
+  res.status(201).json({
+    message: "user registered!",
+    user,
+  });
+});
 
-router.post('/login', async(req, res) => {
-    const { username, password } = req.body;
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
 
-    const isUserExists = await userModel.findOne({
-        username: username
-    })
+  const isUserExists = await userModel.findOne({
+    username: username,
+  });
 
-    if (!isUserExists) {
-        return res.status(401).json({
-            message: "user account not found [invalid username]"
-        })
-    }
-})
+  if (!isUserExists) {
+    return res.status(401).json({
+      message: "user account not found [invalid username]",
+    });
+  }
+
+  const isPasswordValid = password == isUserExists.password;
+  if (!isPasswordValid) {
+    return res.status(401).json({
+      message: "Invalid password",
+    });
+  }
+
+  res.status(200).json({
+    message: 'user loggedIn successfully!'
+  })
+});
 
 module.exports = router;
