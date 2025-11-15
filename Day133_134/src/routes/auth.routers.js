91d97f662b4e4f0deb19472c3829d33087c627b3
SUDHANSHU GHOSH <shudhanshukumar9713@gmail.com>
2025-11-15 20:27:37 +0530

    day 135 token creation, and cookies install then store token in cookies using Postman

--- Day133_134/src/routes/auth.routers.js
+++ Day133_134/src/routes/auth.routers.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const userModel = require("../models/user.models.js");
+const jwt = require('jsonwebtoken');
 const router = express.Router();
 
 /*
@@ -16,9 +17,15 @@ router.post("/register", async (req, res) => {
     password,
   });
 
+  const token = jwt.sign({
+    id:user._id,
+  }, process.env.JWT_SECRET)
+
+  res.cookie('token', token)
+
   res.status(201).json({
     message: "user registered!",
-    user,
+    user
   });
 });
 
@@ -46,5 +53,31 @@ router.post("/login", async (req, res) => {
     message: 'user loggedIn successfully!'
   })
 });
+
+router.get('/user', async(req, res) => {
+  const { token } = req.cookies
+
+  if (!token) {
+    return res.status(401).json({
+      message: "Unauthorized"
+    })
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+    const user = await userModel.findOne({
+      _id:decoded.id
+    }).select('-password -__v')
+    res.status(200).json({
+      message: "User data fetched successfully",
+      user
+    })
+  } catch (error) {
+    return res.status(401).json({
+      message: "Invalid token"
+    })
+  }
+
+})
 
 module.exports = router;
